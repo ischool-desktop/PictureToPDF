40f3098b7aa11fca77d3d3ce86cd21f14f6cfcca
imcloudwu <imcloudwu@gmail.com>
2015-01-13 09:48:37 +0800

    first version

---
+++ PictureToPDF/MainForm.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace PictureToPDF
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelX1 = new DevComponents.DotNetBar.LabelX();
+            this.txtSourcePath = new System.Windows.Forms.TextBox();
+            this.btnSource = new DevComponents.DotNetBar.ButtonX();
+            this.btnTarget = new DevComponents.DotNetBar.ButtonX();
+            this.txtTargetPath = new System.Windows.Forms.TextBox();
+            this.labelX2 = new DevComponents.DotNetBar.LabelX();
+            this.btnConfirm = new DevComponents.DotNetBar.ButtonX();
+            this.btnExit = new DevComponents.DotNetBar.ButtonX();
+            this.btnPicFolder = new DevComponents.DotNetBar.ButtonX();
+            this.txtPicFolder = new System.Windows.Forms.TextBox();
+            this.labelX3 = new DevComponents.DotNetBar.LabelX();
+            this.SuspendLayout();
+            // 
+            // labelX1
+            // 
+            this.labelX1.BackColor = System.Drawing.Color.Transparent;
+            // 
+            // 
+            // 
+            this.labelX1.BackgroundStyle.Class = "";
+            this.labelX1.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.labelX1.Location = new System.Drawing.Point(13, 13);
+            this.labelX1.Name = "labelX1";
+            this.labelX1.Size = new System.Drawing.Size(341, 23);
+            this.labelX1.TabIndex = 8;
+            this.labelX1.Text = "請選擇設定檔案路徑(*.xls)";
+            // 
+            // txtSourcePath
+            // 
+            this.txtSourcePath.Location = new System.Drawing.Point(13, 43);
+            this.txtSourcePath.Name = "txtSourcePath";
+            this.txtSourcePath.Size = new System.Drawing.Size(435, 25);
+            this.txtSourcePath.TabIndex = 1;
+            this.txtSourcePath.TextChanged += new System.EventHandler(this.txtSourcePath_TextChanged);
+            // 
+            // btnSource
+            // 
+            this.btnSource.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.btnSource.BackColor = System.Drawing.Color.Transparent;
+            this.btnSource.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.btnSource.Location = new System.Drawing.Point(373, 13);
+            this.btnSource.Name = "btnSource";
+            this.btnSource.Size = new System.Drawing.Size(75, 23);
+            this.btnSource.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.btnSource.TabIndex = 0;
+            this.btnSource.Text = "選擇";
+            this.btnSource.Click += new System.EventHandler(this.btnSource_Click);
+            // 
+            // btnTarget
+            // 
+            this.btnTarget.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.btnTarget.BackColor = System.Drawing.Color.Transparent;
+            this.btnTarget.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.btnTarget.Location = new System.Drawing.Point(373, 135);
+            this.btnTarget.Name = "btnTarget";
+            this.btnTarget.Size = new System.Drawing.Size(75, 23);
+            this.btnTarget.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.btnTarget.TabIndex = 2;
+            this.btnTarget.Text = "選擇";
+            this.btnTarget.Click += new System.EventHandler(this.btnTarget_Click);
+            // 
+            // txtTargetPath
+            // 
+            this.txtTargetPath.Location = new System.Drawing.Point(13, 165);
+            this.txtTargetPath.Name = "txtTargetPath";
+            this.txtTargetPath.Size = new System.Drawing.Size(435, 25);
+            this.txtTargetPath.TabIndex = 3;
+            this.txtTargetPath.TextChanged += new System.EventHandler(this.txtTargetPath_TextChanged);
+            // 
+            // labelX2
+            // 
+            this.labelX2.BackColor = System.Drawing.Color.Transparent;
+            // 
+            // 
+            // 
+            this.labelX2.BackgroundStyle.Class = "";
+            this.labelX2.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.labelX2.Location = new System.Drawing.Point(13, 135);
+            this.labelX2.Name = "labelX2";
+            this.labelX2.Size = new System.Drawing.Size(341, 23);
+            this.labelX2.TabIndex = 9;
+            this.labelX2.Text = "請選擇輸出目的資料夾";
+            // 
+            // btnConfirm
+            // 
+            this.btnConfirm.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.btnConfirm.BackColor = System.Drawing.Color.Transparent;
+            this.btnConfirm.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.btnConfirm.Location = new System.Drawing.Point(290, 210);
+            this.btnConfirm.Name = "btnConfirm";
+            this.btnConfirm.Size = new System.Drawing.Size(75, 23);
+            this.btnConfirm.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.btnConfirm.TabIndex = 6;
+            this.btnConfirm.Text = "確認";
+            this.btnConfirm.Click += new System.EventHandler(this.btnConfirm_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.btnExit.BackColor = System.Drawing.Color.Transparent;
+            this.btnExit.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.btnExit.Location = new System.Drawing.Point(373, 210);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.btnExit.TabIndex = 7;
+            this.btnExit.Text = "離開";
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnPicFolder
+            // 
+            this.btnPicFolder.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.btnPicFolder.BackColor = System.Drawing.Color.Transparent;
+            this.btnPicFolder.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.btnPicFolder.Location = new System.Drawing.Point(372, 74);
+            this.btnPicFolder.Name = "btnPicFolder";
+            this.btnPicFolder.Size = new System.Drawing.Size(75, 23);
+            this.btnPicFolder.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.btnPicFolder.TabIndex = 4;
+            this.btnPicFolder.Text = "選擇";
+            this.btnPicFolder.Click += new System.EventHandler(this.btnPicFolder_Click);
+            // 
+            // txtPicFolder
+            // 
+            this.txtPicFolder.Location = new System.Drawing.Point(12, 104);
+            this.txtPicFolder.Name = "txtPicFolder";
+            this.txtPicFolder.Size = new System.Drawing.Size(435, 25);
+            this.txtPicFolder.TabIndex = 5;
+            this.txtPicFolder.TextChanged += new System.EventHandler(this.txtPicFolder_TextChanged);
+            // 
+            // labelX3
+            // 
+            this.labelX3.BackColor = System.Drawing.Color.Transparent;
+            // 
+            // 
+            // 
+            this.labelX3.BackgroundStyle.Class = "";
+            this.labelX3.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.labelX3.Location = new System.Drawing.Point(12, 74);
+            this.labelX3.Name = "labelX3";
+            this.labelX3.Size = new System.Drawing.Size(341, 23);
+            this.labelX3.TabIndex = 10;
+            this.labelX3.Text = "請選擇圖片來源資料夾";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(461, 240);
+            this.Controls.Add(this.btnPicFolder);
+            this.Controls.Add(this.txtPicFolder);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnConfirm);
+            this.Controls.Add(this.btnTarget);
+            this.Controls.Add(this.txtTargetPath);
+            this.Controls.Add(this.btnSource);
+            this.Controls.Add(this.txtSourcePath);
+            this.Controls.Add(this.labelX3);
+            this.Controls.Add(this.labelX2);
+            this.Controls.Add(this.labelX1);
+            this.DoubleBuffered = true;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "設定";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevComponents.DotNetBar.LabelX labelX1;
+        private System.Windows.Forms.TextBox txtSourcePath;
+        private DevComponents.DotNetBar.ButtonX btnSource;
+        private DevComponents.DotNetBar.ButtonX btnTarget;
+        private System.Windows.Forms.TextBox txtTargetPath;
+        private DevComponents.DotNetBar.LabelX labelX2;
+        private DevComponents.DotNetBar.ButtonX btnConfirm;
+        private DevComponents.DotNetBar.ButtonX btnExit;
+        private DevComponents.DotNetBar.ButtonX btnPicFolder;
+        private System.Windows.Forms.TextBox txtPicFolder;
+        private DevComponents.DotNetBar.LabelX labelX3;
+    }
+}
+
---
+++ PictureToPDF/MainForm.cs
@@ -0,0 +1,324 @@
+﻿using Aspose.Cells;
+using Aspose.Words;
+using Aspose.Words.Reporting;
+using FISCA.Presentation.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PictureToPDF
+{
+    public partial class MainForm : BaseForm
+    {
+        string _source, _target, _picFolderPath, _templatePath;
+        BackgroundWorker _BW;
+        Workbook _WB;
+        List<string> formats = new List<string>() { ".jpg", ".png" };
+
+        public MainForm()
+        {
+            InitializeComponent();
+            Aspose.Cells.License License_cell = new Aspose.Cells.License();
+            Aspose.Words.License License_word = new Aspose.Words.License();
+
+            MemoryStream stream = new MemoryStream(Properties.Resources.Aspose_Total);
+
+            License_cell.SetLicense(stream);
+            stream.Seek(0, SeekOrigin.Begin);
+            License_word.SetLicense(stream);
+
+            _BW = new BackgroundWorker();
+            _BW.DoWork += new DoWorkEventHandler(_BW_DoWork);
+            _BW.RunWorkerCompleted += new RunWorkerCompletedEventHandler(_BW_RunWorkerCompleted);
+        }
+
+        private void _BW_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Error != null)
+                MessageBox.Show(e.Error.Message);
+            else
+                System.Diagnostics.Process.Start(_target);
+        }
+
+        private void _BW_DoWork(object sender, DoWorkEventArgs e)
+        {
+            using (FileStream stream = new FileStream(_templatePath, FileMode.Open))
+            {
+                DataTable table = GetData();
+                foreach (DataRow row in table.Rows)
+                {
+                    stream.Seek(0, SeekOrigin.Begin);
+
+                    string fileName = _target + "\\" + row["學號"] + "_" + row["學生姓名"] + ".pdf";
+
+                    Document doc = new Document(stream);
+                    doc.MailMerge.FieldMergingCallback = new MergeCallBack();
+
+                    doc.MailMerge.Execute(row);
+
+                    doc.Save(fileName, Aspose.Words.SaveFormat.Pdf);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 選擇來源路徑
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSource_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+
+            open.Filter = "Excel檔案 (*.xls)|*.xls|所有檔案 (*.*)|*.*";
+
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                this.txtSourcePath.Text = open.FileName;
+                this._source = open.FileName;
+            }
+        }
+
+        /// <summary>
+        /// 選擇目的目錄
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnTarget_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                this.txtTargetPath.Text = fbd.SelectedPath;
+                _target = fbd.SelectedPath;
+            }
+        }
+
+        /// <summary>
+        /// 選擇圖片來源目錄
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnPicFolder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                this.txtPicFolder.Text = fbd.SelectedPath;
+                _picFolderPath = fbd.SelectedPath;
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnConfirm_Click(object sender, EventArgs e)
+        {
+            //檢查路徑設定
+            if (string.IsNullOrWhiteSpace(_source) || string.IsNullOrWhiteSpace(_target) || string.IsNullOrWhiteSpace(_picFolderPath))
+            {
+                MessageBox.Show("請確認三種路徑皆已設定");
+                return;
+            }
+
+            //檢查template存在
+            _templatePath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + "\\template.doc";
+            if (!File.Exists(_templatePath))
+            {
+                MessageBox.Show("找不到檔案:" + _templatePath);
+                return;
+            }
+                
+            //嘗試讀取Excel檔
+            try
+            {
+                _WB = new Workbook(_source);
+            }
+            catch(Exception error)
+            {
+                MessageBox.Show(error.Message);
+                return;
+            }
+
+            if (_BW.IsBusy)
+                MessageBox.Show("正在忙碌中,請稍後再試...");
+            else
+                _BW.RunWorkerAsync();
+        }
+
+        private DataTable GetData()
+        {
+            //Column對照
+            Dictionary<string, int> columnMapping = new Dictionary<string, int>();
+            columnMapping.Add("學號", -1);
+            columnMapping.Add("學生姓名", -1);
+            columnMapping.Add("學年度學期", -1);
+            columnMapping.Add("指導老師", -1);
+            columnMapping.Add("實習老師", -1);
+            columnMapping.Add("圖片路徑", -1);
+
+            //DataTable欄位建立
+            DataTable table = new DataTable();
+            table.Columns.Add("學號");
+            table.Columns.Add("學生姓名");
+            table.Columns.Add("學年度學期");
+            table.Columns.Add("指導老師");
+            table.Columns.Add("實習老師");
+            table.Columns.Add("圖片路徑");
+
+            //取得有資料的最大範圍
+            int maxDataCloumn = _WB.Worksheets[0].Cells.MaxDataColumn;
+            int MaxDataRow = _WB.Worksheets[0].Cells.MaxDataRow;
+
+            //記憶支援的欄位index
+            for (int i = 0; i <= maxDataCloumn; i++)
+            {
+                string columnName = _WB.Worksheets[0].Cells[0, i].Value + "";
+
+                if (columnMapping.ContainsKey(columnName))
+                    columnMapping[columnName] = i;
+            }
+
+            //取得每個row
+            for (int i = 1; i <= MaxDataRow; i++)
+            {
+                DataRow row = table.NewRow();
+
+                foreach (string columnName in columnMapping.Keys)
+                {
+                    int columnIndex = columnMapping[columnName];
+
+                    if (columnIndex >= 0)
+                    {
+                        string value = _WB.Worksheets[0].Cells[i, columnIndex].Value + "";
+
+                        switch (columnName)
+                        {
+                            case "學號":
+                                row["學號"] = value;
+                                break;
+
+                            case "學生姓名":
+                                row["學生姓名"] = value;
+                                break;
+
+                            case "學年度學期":
+                                row["學年度學期"] = value;
+                                break;
+
+                            case "指導老師":
+                                row["指導老師"] = value;
+                                break;
+
+                            case "實習老師":
+                                row["實習老師"] = value;
+                                break;
+
+                            case "圖片路徑":
+                                foreach (string f in formats)
+                                {
+                                    string path = _picFolderPath + "\\" + value + f;
+                                    if (File.Exists(path))
+                                    {
+                                        row["圖片路徑"] = path;
+                                        break;
+                                    }
+                                }
+                                break;
+                        }
+                    }
+                }
+
+                table.Rows.Add(row);
+            }
+
+            return table;
+        }
+
+        private void txtSourcePath_TextChanged(object sender, EventArgs e)
+        {
+            _source = txtSourcePath.Text;
+        }
+
+        private void txtTargetPath_TextChanged(object sender, EventArgs e)
+        {
+            _target = txtTargetPath.Text;
+        }
+
+        private void txtPicFolder_TextChanged(object sender, EventArgs e)
+        {
+            _picFolderPath = txtPicFolder.Text;
+        }
+
+        class MergeCallBack : IFieldMergingCallback
+        {
+            public void FieldMerging(FieldMergingArgs args)
+            {
+                //do nothing...
+            }
+
+            public void ImageFieldMerging(ImageFieldMergingArgs args)
+            {
+                string filePath = args.FieldValue + "";
+                if (!string.IsNullOrWhiteSpace(filePath))
+                {
+                    args.Image = GetBitmap(filePath, 300 , 200);
+                }
+            }
+
+            /// <summary>
+            /// 取得比例縮小的圖片
+            /// </summary>
+            /// <param name="filePath"></param>
+            /// <param name="maxWidth"></param>
+            /// <param name="maxHeight"></param>
+            /// <returns></returns>
+            public static Bitmap GetBitmap(string filePath, int maxWidth, int maxHeight)
+            {
+                //照理說不會爆
+                Bitmap photo = new Bitmap(filePath);
+
+                int width = photo.Width;
+                int height = photo.Height;
+                Size newSize;
+
+                if (width < maxWidth && height < maxHeight)
+                    return photo;
+
+                decimal maxW = Convert.ToDecimal(maxWidth);
+                decimal maxH = Convert.ToDecimal(maxHeight);
+
+                decimal mp = decimal.Divide(maxW, maxH);
+                decimal p = decimal.Divide(width, height);
+
+                // 若長寬比預設比例較寬, 則以傳入之長為縮放基準
+                if (mp > p)
+                {
+                    decimal hp = decimal.Divide(maxH, height);
+                    decimal newWidth = decimal.Multiply(hp, width);
+                    newSize = new Size(decimal.ToInt32(newWidth), maxHeight);
+                }
+                else
+                {
+                    decimal wp = decimal.Divide(maxW, width);
+                    decimal newHeight = decimal.Multiply(wp, height);
+                    newSize = new Size(maxWidth, decimal.ToInt32(newHeight));
+                }
+
+                return new Bitmap(photo, newSize);
+            }
+        }
+    }
+}
